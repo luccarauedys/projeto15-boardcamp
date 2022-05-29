e769bd3b77a14db611e4dc6b697fe7a358755ec0
Lucca Rauédys <luccarauedys@outlook.com>
2022-05-29 11:43:48 -0300

    importando o customersRouter no index.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Boardcamp-API
+
+>This Project is a game rental management system, built with the objective of practicing the knowledge acquired in Relational Database (SQL).
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ dotenv.config();
 
 import categoriesRouter from "./routes/categoriesRouter.js";
 import gamesRouter from "./routes/gamesRouter.js";
+import customersRouter from "./routes/customersRouter.js";
 
 const app = express().use(cors()).use(express.json());
 
 app.use(categoriesRouter);
 app.use(gamesRouter);
+app.use(customersRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
