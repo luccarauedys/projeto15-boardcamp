9c6f437f108980f33030ec8a9a0e6da021aafa9d
Lucca Rauédys <luccarauedys@outlook.com>
2022-05-29 11:48:15 -0300

    corrigindo erro em customersSchema

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const customersSchema = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.string().pattern(new RegExp("^[0-9]{10,11})$")).required(),
+  phone: Joi.string().pattern(new RegExp("^[0-9]{10,11}$")).required(),
   cpf: Joi.string().pattern(new RegExp("^[0-9]{11}$")).required(),
   birthday: Joi.string()
     .pattern(new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$"))
